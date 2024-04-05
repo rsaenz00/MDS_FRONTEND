4c655e6838226bddb58127c5a2dfd96596307ec2
htorresanna <htorres@doctormas.com.pe>
2024-04-05 12:33:25 -0500

    Mantenedor blog

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,18 +3,21 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ViewsRoutes } from './views-routing.module';
 import { MaterialModule } from '../material.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StarterComponent } from './layout/starter/starter.component';
+import { BandejaComponent } from './layout/bandeja/bandeja.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [    
+  ],
   imports: [
     CommonModule,
     MaterialModule,
     FormsModule,
     RouterModule.forChild(ViewsRoutes),
-    StarterComponent
+    StarterComponent,
+    BandejaComponent
   ]
 })
 export class ViewsModule { }
