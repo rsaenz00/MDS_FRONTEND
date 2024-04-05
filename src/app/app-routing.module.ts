16b13168125efb159e648b041da5e20135ec480a
htorresanna <htorres@doctormas.com.pe>
2024-04-05 12:34:34 -0500

    Mantenedor blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FullComponent } from './views/layout/full/full.component';
 import { BlankComponent } from './views/layout/blank/blank.component';
+import { BandejaComponent } from './views/layout/bandeja/bandeja.component';
 
 const routes: Routes = [
-{
-  path: '',
+  {
+    path: '',
     component: FullComponent,
     children: [
       {
@@ -18,19 +19,26 @@ const routes: Routes = [
         loadChildren: () =>
           import('./views/views.module').then((m) => m.ViewsModule),
       },
+      {
+        path: 'bandeja',
+        component: BandejaComponent,
+        data: {
+          title: 'Bandeja de atenciones'
+        },
+      },
     ],
-},
-{
-  path: '',
-  component: BlankComponent,
-  children: [
-    {
-      path: 'auth',
-      loadChildren: () =>
-        import('./views/auth/auth.module').then((m) => m.AuthModule),
-    },
-  ],
-},
+  },
+  {
+    path: '',
+    component: BlankComponent,
+    children: [
+      {
+        path: 'auth',
+        loadChildren: () =>
+          import('./views/auth/auth.module').then((m) => m.AuthModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
