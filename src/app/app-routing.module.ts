c336760b7b6af24247017e383e26e34d2ba67b39
htorresanna <htorres@doctormas.com.pe>
2024-05-10 12:29:38 -0500

    Avances de front al 10 - 05 - 2024

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,69 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FullComponent } from './views/layout/full/full.component';
 import { BlankComponent } from './views/layout/blank/blank.component';
-import { BandejaComponent } from './views/layout/bandeja/bandeja.component';
-//import { MadBandejaComponent } from './views/layout/mad-bandeja/mad-bandeja.component';
-//import { MadNuevatencionComponent } from './views/layout/mad-nuevatencion/mad-nuevatencion.component';
-//import { SctrBandejaComponent } from './views/layout/sctr-bandeja/sctr-bandeja.component';
 import { AuthGuard } from './helpers/security/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>import('./modules/modules.module').then((m) => m.ModulesModule),
-    /*component: FullComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'starter',
-        pathMatch: 'full',
-      },
-      {
-        path: 'starter',
-        loadChildren: () =>
-          import('./views/views.module').then((m) => m.ViewsModule),
-      },
-      {
-        path: 'bandeja',
-        component: BandejaComponent,
-        data: {
-          title: 'Bandeja de atenciones'
-        },
-      },
-      {
-        path: 'bandejamad',
-        component: MadBandejaComponent,
-        data: {
-          title: 'Bandeja de atenciones m�dico a domicilio (MAD)'
-        },
-      },
-      {
-        path: 'nuevatencionmad',
-        component: MadNuevatencionComponent,
-        data: {
-          title: 'Nueva atenci�n m�dica a domicilio (MAD)'
-        },
-      },
-      {
-        path: 'bandejasctr',
-        component: SctrBandejaComponent,
-        data: {
-          title: 'Bandeja de atenciones SCTR'
-        },
-      },
-    ],*/
-  },
-  /*{
-    path: '',
-    component: BlankComponent,
-    children: [
-      {
-        path: 'auth',
-        loadChildren: () =>
-          import('./views/auth/auth.module').then((m) => m.AuthModule),
-      },
-    ],
-  },*/
+    loadChildren: () => import('./modules/modules.module').then((m) => m.ModulesModule)
+  }
 ];
 
 @NgModule({
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -14,32 +14,6 @@ import { ProductsComponent } from './layout/full/shared/products/products.compon
 import { PaymentGatewaysComponent } from './layout/full/shared/payment-gateways/payment-gateways.component';
 import { TopProjectsComponent } from './layout/full/shared/top-projects/top-projects.component';
 import { CommonDialogComponent } from './layout/full/shared/common-dialog/common-dialog.component';
-import { BandejaComponent } from './layout/bandeja/bandeja.component';
-import { SoporteComponent } from './layout/soporte/soporte.component';
-import { MadBandejaComponent } from './layout/mad-bandeja/mad-bandeja.component';
-import { MadSeguimientoComponent } from './layout/mad-seguimiento/mad-seguimiento.component';
-import { MadIncidenciaComponent } from './layout/mad-incidencia/mad-incidencia.component';
-import { MadAuditoriatencionComponent } from './layout/mad-auditoriatencion/mad-auditoriatencion.component';
-import { MadSoporteComponent } from './layout/mad-soporte/mad-soporte.component';
-import { MadMostrardatosComponent } from './layout/mad-mostrardatos/mad-mostrardatos.component';
-import { MadIngresarcodigosComponent } from './layout/mad-ingresarcodigos/mad-ingresarcodigos.component';
-import { MadDatoscomprobantepagoComponent } from './layout/mad-datoscomprobantepago/mad-datoscomprobantepago.component';
-import { MadHistorialencuestaComponent } from './layout/mad-historialencuesta/mad-historialencuesta.component';
-import { MadEditarservicioComponent } from './layout/mad-editarservicio/mad-editarservicio.component';
-import { MadDatospacienteComponent } from './layout/mad-datospaciente/mad-datospaciente.component';
-import { MadSitedsComponent } from './layout/mad-siteds/mad-siteds.component';
-import { MadDireccionesComponent } from './layout/mad-direcciones/mad-direcciones.component';
-import { MadNuevatencionComponent } from './layout/mad-nuevatencion/mad-nuevatencion.component';
-import { SctrBandejaComponent } from './layout/sctr-bandeja/sctr-bandeja.component';
-import { SctrSeguimientoComponent } from './layout/sctr-seguimiento/sctr-seguimiento.component';
-import { SctrVerdatosComponent } from './layout/sctr-verdatos/sctr-verdatos.component';
-import { SctrSoporteComponent } from './layout/sctr-soporte/sctr-soporte.component';
-import { SctrTiposervicioComponent } from './layout/sctr-tiposervicio/sctr-tiposervicio.component';
-import { SctrNuevatencionComponent } from './layout/sctr-nuevatencion/sctr-nuevatencion.component';
-import { SctrRegistramotivoComponent } from './layout/sctr-registramotivo/sctr-registramotivo.component';
-import { SctrMantenimientoclinicaComponent } from './layout/sctr-mantenimientoclinica/sctr-mantenimientoclinica.component';
-import { ListadoubigeosComponent } from './layout/listadoubigeos/listadoubigeos.component';
-import { ListadoclinicasComponent } from './layout/listadoclinicas/listadoclinicas.component';
 
 @NgModule({
   declarations: [
@@ -59,33 +33,7 @@ import { ListadoclinicasComponent } from './layout/listadoclinicas/listadoclinic
     MaterialModule,
     FormsModule,
     RouterModule.forChild(ViewsRoutes),
-    StarterComponent,
-    BandejaComponent,
-    SoporteComponent,
-    MadBandejaComponent,
-    MadSeguimientoComponent,
-    MadIncidenciaComponent,
-    MadAuditoriatencionComponent,
-    MadSoporteComponent,
-    MadMostrardatosComponent,
-    MadIngresarcodigosComponent,
-    MadDatoscomprobantepagoComponent,
-    MadHistorialencuestaComponent,
-    MadEditarservicioComponent,
-    MadDatospacienteComponent,
-    MadSitedsComponent,
-    MadDireccionesComponent,
-    MadNuevatencionComponent,
-    SctrBandejaComponent,
-    SctrSeguimientoComponent,
-    SctrVerdatosComponent,
-    SctrSoporteComponent,
-    SctrTiposervicioComponent,
-    SctrNuevatencionComponent,
-    SctrRegistramotivoComponent,
-    SctrMantenimientoclinicaComponent,
-    ListadoubigeosComponent,
-    ListadoclinicasComponent
+    StarterComponent
   ]
 })
 export class ViewsModule { }
